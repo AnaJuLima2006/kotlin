70fe021898a2cd5269f08f7b3ab39f679147aab2
najuprogrammer <105178561+AnaJuLima2006@users.noreply.github.com>
2025-03-11 10:28:36 -0300

    Add files via upload

---
+++ idade.kt
@@ -0,0 +1,17 @@
+fun main  (){
+    //Hora de Codar Um!!! exercico 3
+    //Escreva um programa em que o usuário informe o seu nome e em seguida o programa
+    //perguntará a idade do usuário. Agora o programa deve exibir
+    //a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".
+
+    print("Digite o seu nome:")
+
+    var nome = readln()
+
+    print("Digite o sua idade:")
+
+    var idade = readln().toInt()
+
+    print("Olá  $nome, sua idade é $idade")
+
+}
