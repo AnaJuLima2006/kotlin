3411605b6d64df24c7b079f4c326eda2dec55aa2
najuprogrammer <105178561+AnaJuLima2006@users.noreply.github.com>
2025-03-11 10:15:07 -0300

    Add files via upload

---
+++ nome.kt
@@ -0,0 +1,11 @@
+fun main  (){
+    //Hora de Codar Um!!! exercico 2
+    //Escreva um programa em que o usuário informe
+    //o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+    print("Digite o seu nome:")
+
+    var nome = readln()
+
+    print("Olá  $nome!")
+
+}
