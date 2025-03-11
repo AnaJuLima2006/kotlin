c0c6491581813bc3bf7743b6761bd6b0cedbf40f
najuprogrammer <105178561+AnaJuLima2006@users.noreply.github.com>
2025-03-11 10:16:48 -0300

    Update carro.kt

--- carro.kt
+++ carro.kt
@@ -1,3 +1,6 @@
+//Hora de Codar Um!!! exercico 1
+//Escreva um programa que crie uma variável chamada "nome_do_carro" 
+//e atribua-a um valor "Fusca". Exiba-a ao usuário.
 fun main  (){
     var nome_do_carro = "Fusca"
 
