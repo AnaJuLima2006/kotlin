97a87a362e3a734a541d8a43ecd0fcf57e60fee8
najuprogrammer <105178561+AnaJuLima2006@users.noreply.github.com>
2025-03-12 10:43:48 -0300

    Add files via upload

---
+++ media.kt
@@ -0,0 +1,26 @@
+//Hora de Codar Dois!!! Exercicio 5
+//Faça um programa que leia 6 valores informados pelo usuário, calcule,
+//exiba os números informados e escreva a média aritmética desses valores lidos.
+fun main(){
+
+    var media: Double
+    var soma : Double
+
+    print("Digite o primeiro número: ")
+    var numeroUm = readln().toDouble()
+    print("Digite o segundo número: ")
+    var numeroDois = readln().toDouble()
+    print("Digite o terceiro número: ")
+    var numeroTres = readln().toDouble()
+    print("Digite o primeiro número: ")
+    var numeroQuatro = readln().toDouble()
+    print("Digite o segundo número: ")
+    var numeroCinco = readln().toDouble()
+    print("Digite o terceiro número: ")
+    var numeroSeis = readln().toDouble()
+
+    soma = numeroUm + numeroDois + numeroTres + numeroQuatro + numeroCinco + numeroSeis
+    media = soma/6
+
+    println("A média aritmédica entre os seis números é:  $media")
+}
