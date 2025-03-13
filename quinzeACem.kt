eac2fe9165f1173f29495fffbfbac89441d5813b
najuprogrammer <105178561+AnaJuLima2006@users.noreply.github.com>
2025-03-12 21:58:01 -0300

    Add files via upload

---
+++ quinzeACem.kt
@@ -0,0 +1,12 @@
+//Hora de Codar Três, Exercício Quatro!!
+//Faça um algoritmo que calcule e escreva a mídia aritmética
+//dos números inteiros entre 15 (inclusive) e 100 (inclusive).
+
+fun main(){
+    val a1 = 15 //primeiro termo
+    val an = 100 //ultimo termo
+
+    val media = (a1 + an) / 2.0 //foi usado a formula da média de uma progressão aritmética
+    print("A média aritmética dos números entre $a1 e $an é: $media")
+
+}
