19d6468d77f0ce0d38a7dd82b6179cff5ef8939f
najuprogrammer <105178561+AnaJuLima2006@users.noreply.github.com>
2025-03-12 23:21:58 -0300

    Add files via upload

---
+++ maioresQueCem.kt
@@ -0,0 +1,19 @@
+//Hora de Codar Três, Exercício Oito!!
+//Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive).
+//Considere que o N será sempre maior que ZERO. N é um valor informado pelo usuário
+
+
+fun main(){
+    var n : Int
+
+    println("10 primeiros números maiores que 100: ")
+
+    for (i in 101.. 110)  {
+        print(i)
+
+        if (i < 110) {
+            print(", ")
+        }
+    }
+
+}
